dfe46dc63a58f2fbbb74dbe98262c0060cac4225
Cesar Fernando Montealegre Correa <cesarmontealegere@gmail.com>
2023-05-13 22:44:48 -0500

    Update db_Myapp.sql

--- db_Myapp.sql
+++ db_Myapp.sql
@@ -4,9 +4,10 @@ USE db_Myapp;
 
 CREATE TABLE
   users_tbl (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT AUTO_INCREMENT,
     user_firstname VARCHAR(40) NOT NULL,
     user_lastname VARCHAR(40) NOT NULL,
     user_email VARCHAR(60) NOT NULL,
-    user_password VARBINARY(256)
+    user_password VARBINARY(256),
+    PRIMARY KEY (user_id)
 );
